ce952cae3a353bba95b39e359ff1b32a85beac81
Anna Golosova <golosova76@gmail.com>
2024-05-03 21:41:12 +0300

    feat: add configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    // es6: true,
+  },
+  ignorePatterns: [".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  extends: ["airbnb-base", "airbnb-typescript/base", "plugin:prettier/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+      },
+    ],
+    "@typescript-eslint/dot-notation": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: { alwaysTryTypes: true },
+      node: {
+        extensions: [".js", ".jsx", ".json", ".scss", ".html", ".ts", ".tsx"],
+      },
+    },
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  noInlineConfig: false,
+};
