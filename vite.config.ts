5ddc70c850be442d6df60a7d3226f0502a07e848
Anna Golosova <golosova76@gmail.com>
2024-05-04 13:04:18 +0300

    Development environment configuration (#46)
    * feat: add configure a Vite
    
    * feat: add configure a TypeScript
    
    * feat: add configure ESLint
    
    * feat: add configure Prettier
    
    * feat: add configure StyleLint
    
    * feat: add configure Commitizen (#41)
    
    * feat: add configure Husky (#44)
    
    * feat: add configure Jest (#45)
    
    * Update tsconfig.json

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default {
+  base: "./",
+  build: {
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
+  plugins: [tsconfigPaths()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {},
+};
