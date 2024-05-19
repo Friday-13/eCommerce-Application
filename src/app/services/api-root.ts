bfc21f13c13068e1f6437b2a40bfa47836b33c96
DmitrySamsonenko <samsondm97@gmail.com>
2024-05-20 00:23:51 +0300

    feat: add loading client configs from .env

--- src/app/services/api-root.ts
+++ src/app/services/api-root.ts
@@ -1,11 +1,9 @@
-import ctpClient from '@services/client';
+import { clientConfig, ctpClient } from '@services/client';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-
-const projectKey = 'codecraft';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: `${projectKey}`,
+  projectKey: clientConfig.VITE_CTP_PROJECT_KEY,
 });
 
 // Example call to return Project information
@@ -17,33 +15,35 @@ const getProject = () => {
 // Retrieve Project information and output the result to the log
 getProject().then(console.log).catch(console.error);
 
-// apiRoot
-//   .login()
-//   .post({
-//     // The CustomerDraft is the object within the body
-//     body: {
-//       email: 'sxxdk@example.com',
-//       password: 'examplePassword',
-//     },
-//   })
-//   .execute()
-//
-//   .then(({ body }) => {
-//     console.log(body.customer);
-//   })
-//   .catch(console.error);
-
 apiRoot
-  .customers()
+  .login()
   .post({
     // The CustomerDraft is the object within the body
     body: {
-      email: 'sxxdk@example.com',
+      email: 'hehehe@example.com',
       password: 'examplePassword',
     },
   })
   .execute()
+
   .then(({ body }) => {
     console.log(body.customer);
   })
   .catch(console.error);
+
+// apiRoot
+//   .customers()
+//   .post({
+//     // The CustomerDraft is the object within the body
+//     body: {
+//       email: 'hehehe@example.com',
+//       password: 'examplePassword',
+//     },
+//   })
+//   .execute()
+//   .then(({ body }) => {
+//     console.log(body.customer);
+//   })
+//   .catch(console.error);
+//
+console.log(clientConfig);
--- src/app/services/client.ts
+++ src/app/services/client.ts
@@ -7,36 +7,24 @@ import {
   type HttpMiddlewareOptions, // Required for sending HTTP requests
 } from '@commercetools/sdk-client-v2';
 
-const projectKey = 'codecraft';
-const scopes = [
-  'manage_customer_groups:codecraft',
-  'manage_cart_discounts:codecraft',
-  'manage_project_settings:codecraft',
-  'manage_customers:codecraft',
-  'manage_my_profile:codecraft',
-  'manage_order_edits:codecraft',
-  'manage_my_orders:codecraft',
-  'manage_tax_categories:codecraft',
-  'manage_shipping_methods:codecraft',
-  'manage_products:codecraft',
-  'manage_payments:codecraft',
-  'create_anonymous_token:codecraft',
-  'manage_categories:codecraft',
-  'manage_my_shopping_lists:codecraft',
-  'manage_types:codecraft',
-  'manage_my_payments:codecraft',
-  'manage_orders:codecraft',
-  'manage_extensions:codecraft',
-  'manage_shopping_lists:codecraft',
-  'manage_discount_codes:codecraft',
-];
-const region = 'europe-west1.gcp';
-const clientID = 'CoCgAlV-gfIpaZaIbTlUAhq6';
-const clientSecret = 'trN5-CHITl9NLW8YPu793dWlgke6Re4G';
+interface IClientConfig extends ImportMetaEnv {
+  VITE_CTP_API_URL: string;
+  VITE_CTP_AUTH_URL: string;
+  VITE_CTP_CLIENT_ID: string;
+  VITE_CTP_CLIENT_SECRET: string;
+  VITE_CTP_PROJECT_KEY: string;
+  VITE_CTP_SCOPES: string;
+}
+export const clientConfig = import.meta.env as IClientConfig;
+
+const projectKey = clientConfig.VITE_CTP_PROJECT_KEY;
+const scopes = clientConfig.VITE_CTP_SCOPES.split(' ');
+const clientID = clientConfig.VITE_CTP_CLIENT_ID;
+const clientSecret = clientConfig.VITE_CTP_CLIENT_SECRET;
 
 // Configure authMiddlewareOptions
 const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: `https://auth.${region}.commercetools.com`,
+  host: clientConfig.VITE_CTP_AUTH_URL,
   projectKey,
   credentials: {
     clientId: `${clientID}`,
@@ -48,16 +36,14 @@ const authMiddlewareOptions: AuthMiddlewareOptions = {
 
 // Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: `https://api.${region}.commercetools.com`,
+  host: clientConfig.VITE_CTP_API_URL,
   fetch,
 };
 
 // Export the ClientBuilder
-const ctpClient = new ClientBuilder()
+export const ctpClient = new ClientBuilder()
   .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
   .withClientCredentialsFlow(authMiddlewareOptions)
   .withHttpMiddleware(httpMiddlewareOptions)
   .withLoggerMiddleware() // Include middleware for logging
   .build();
-
-export default ctpClient;
