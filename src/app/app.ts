0173851ac1f37bec1639877326d807bb433cfe26
Anna Golosova <a.golosova@3l.ru>
2024-05-21 16:51:50 +0300

    feat: add change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import Error404 from '@views/404/404';
+import FooterView from '@views/footer/footer';
+import HeaderView from '@views/header/header';
 import LoginView from '@views/login/login-page';
 import MainView from '@views/main/main-page';
 import RegistrationView from '@views/registration/registration-page';
 
 class App {
+  private headerView: HeaderView;
+
+  private footerView: FooterView;  
+  
   private currentView:
     | Error404
     | MainView
@@ -11,31 +17,52 @@ class App {
     | LoginView
     | null = null;
 
+    
+
   constructor() {
+    this.headerView = new HeaderView();
+    this.footerView = new FooterView();
+
     window.addEventListener('hashchange', this.route);
     window.addEventListener('load', this.route);
   }
 
+  private addHeader(): void {
+    if (window.location.hash !== '#error') {        
+      this.headerView.initializeHeader(); 
+      document.body.appendChild(this.headerView.htmlElement);
+    }
+  }
+
+  private addFooter(): void {
+    if (window.location.hash !== '#error') {        
+      this.footerView.initializeFooter(); 
+      document.body.appendChild(this.footerView.htmlElement);
+    }
+  }
+
+  
+
   private route = (): void => {
     if (!window.location.hash) {
-      window.location.hash = '#main';
+      window.location.hash = '#registration';
       return;
     }
     if (this.currentView) {
-      this.currentView.clearContent();
-    }
+      document.body.removeChild(this.currentView.htmlElement);
+      this.currentView = null;
+    }    
+    
 
     switch (window.location.hash) {
       case '#main':
         this.currentView = new MainView();
         break;
       case '#registration':
-        this.currentView = new RegistrationView();
-        document.body.appendChild(this.currentView.htmlElement);
+        this.currentView = new RegistrationView();        
         break;
       case '#login':
-        this.currentView = new LoginView();
-        document.body.appendChild(this.currentView.htmlElement);
+        this.currentView = new LoginView();        
         break;
       case '#error':
         this.currentView = new Error404();
@@ -43,13 +70,20 @@ class App {
         break;
       default:
         this.currentView = new Error404();
-        document.body.appendChild(this.currentView.htmlElement);
+        document.body.appendChild(this.currentView.htmlElement);       
+    }
+
+    if (this.currentView) {      
+      document.body.appendChild(this.currentView.htmlElement);      
     }
   };
 
   public start(): void {
+    this.addHeader();
     this.route();
+    this.addFooter();
   }
+ 
 }
 
 export default App;
--- src/app/views/main/main-page.ts
+++ src/app/views/main/main-page.ts
@@ -1,43 +1,25 @@
 import View from '@views/view';
 import { BaseComponent, IAttributes } from '@components/base-component';
-import FooterView from '@views/footer/footer';
-import HeaderView from '@views/header/header';
 
 export default class MainView extends View {
-  private headerView: HeaderView;
-
-  private footerView: FooterView;
+  private mainContainer!: BaseComponent;  // добавлять разные блоки сюда
 
   constructor() {
     const attrs: IAttributes = {
       tag: 'main',
       classList: ['main'],
     };
-    super(attrs);
-    this.headerView = new HeaderView();
-    this.footerView = new FooterView();
-    this.initializeContent();
+    super(attrs);    
+    this.initializeMainContent();
   }
 
-  private initializeContent(): void {
+  private initializeMainContent(): void {
     const mainContainerAttrs: IAttributes = {
       classList: ['main__container'],
+      content: 'SOS'
     };
-    const mainContainer = new BaseComponent(mainContainerAttrs);
-    this.appendChild(mainContainer);
-
-    this.headerView.initializeHeader();
-    document.body.appendChild(this.headerView.htmlElement);
-
-    document.body.appendChild(this.htmlElement);
-
-    this.footerView.initializeFooter();
-    document.body.appendChild(this.footerView.htmlElement);
-  }
-
-  public clearContent(): void {
-    document.body.removeChild(this.headerView.htmlElement);
-    document.body.removeChild(this.htmlElement);
-    document.body.removeChild(this.footerView.htmlElement);
-  }
+    this.mainContainer = new BaseComponent(mainContainerAttrs);
+    this.appendChild(this.mainContainer);
+    document.body.appendChild(this.htmlElement);    
+  }  
 }
