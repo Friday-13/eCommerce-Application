4dc181b06bec7acc34946424ed7e8ad661ac22d6
dashaborzova <dashaborzova@yandex.ru>
2024-05-24 13:55:01 +0300

    fix(email-validator): add trim() to regex value

--- src/app/utils/validators/email-validator.ts
+++ src/app/utils/validators/email-validator.ts
@@ -3,7 +3,7 @@ import { IValidator } from './validator';
 const emailValidator: IValidator = {
   validateFunction: (value: string): boolean => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(value);
+    return regex.test(value.trim());
   },
   invalidMessage: 'Please enter a valid email address.',
 };
