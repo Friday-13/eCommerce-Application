91e50ae220ea3de4beb1b221f10b3c390da267bd
Anna Golosova <golosova76@gmail.com>
2024-06-01 23:21:37 +0300

    feat: add modal slider

--- src/app/views/product/slider-modal.ts
+++ src/app/views/product/slider-modal.ts
@@ -2,6 +2,7 @@ import { BaseComponent, IAttributes } from '@components/base-component';
 import View from '@views/view';
 import { IImageAttributes, ImageComponent } from '@components/image-component';
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import { IButtonAttributes } from '@components/button-component';
 
 export default class ModalImageSliderProducts extends View {
@@ -9,41 +10,24 @@ export default class ModalImageSliderProducts extends View {
 
   private containerSlideModal!: BaseComponent;
 
+  private initialIndex!: number;
+
   constructor(images: string[], initialIndex: number) {
     const attrs: IAttributes = {
-      classList: ['swiper-modal'],
+      classList: ['swiper swiper-modal'],
     };
     super(attrs);
     console.log('Modal slider initialized with index:', initialIndex); // Для отладки
     this.containerModal = new BaseComponent(attrs);
-    this.initializeSliderModal(images, initialIndex);
+    this.initialIndex = initialIndex;
+    this.initializeSliderModal(images);
   }
 
-  public initializeSliderModal(images: string[], initialIndex: number) {
+  public initializeSliderModal(images: string[]) {
     this.initializeSliderDetailsModal(images);
     this.initializeSliderNavigationModal();
     this.initializeSliderPaginationModal();
     this.initializeSliderCloseModal();
-    setTimeout(() => {
-      this.initializeSwiper(initialIndex);
-    }, 0);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private initializeSwiper(initialIndex: number) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const swiper = new Swiper('.swiper', {
-      initialSlide: initialIndex,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
   }
 
   private initializeSliderDetailsModal(images: string[]) {
@@ -73,7 +57,7 @@ export default class ModalImageSliderProducts extends View {
 
   private initializeSliderNavigationModal() {
     const navigationSliderNextAttrs: IAttributes = {
-      classList: ['swiper-button-next'],
+      classList: ['swiper-button-next next-modal'],
     };
     const navigationSliderModalNext = new BaseComponent(
       navigationSliderNextAttrs
@@ -81,7 +65,7 @@ export default class ModalImageSliderProducts extends View {
     this.containerModal.appendChild(navigationSliderModalNext);
 
     const navigationSliderPrevAttrs: IAttributes = {
-      classList: ['swiper-button-prev'],
+      classList: ['swiper-button-prev prev-modal'],
     };
     const navigationSliderModalPrev = new BaseComponent(
       navigationSliderPrevAttrs
@@ -91,7 +75,7 @@ export default class ModalImageSliderProducts extends View {
 
   private initializeSliderPaginationModal() {
     const paginationSliderAttrs: IAttributes = {
-      classList: ['swiper-pagination'],
+      classList: ['swiper-pagination pagination-modal'],
     };
     const paginationSliderModal = new BaseComponent(paginationSliderAttrs);
     this.containerModal.appendChild(paginationSliderModal);
@@ -108,22 +92,42 @@ export default class ModalImageSliderProducts extends View {
     closeButton.node.addEventListener('click', (event) => {
       event.preventDefault();
       this.close();
+      document.body.removeChild(this.htmlElement);
     });
     this.containerModal.node.addEventListener('click', (event) => {
       const target = event.target as Node;
-      if (!this.containerSlideModal.node.contains(target)) {
-        this.close();
+      if (target && !this.containerSlideModal.node.contains(target)) {
+        // this.close();
       }
     });
   }
 
   public open() {
     this.containerModal.node.classList.add('popup-open');
+    setTimeout(() => {
+      this.initializeSwiper();
+    }, 0);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private initializeSwiper() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const swiper = new Swiper('.swiper', {
+      initialSlide: this.initialIndex,
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
   }
 
   public close() {
     this.containerModal.node.classList.remove('popup-open');
-    // this.container.node.classList.add('hidden');
   }
 
   public get htmlElement() {
