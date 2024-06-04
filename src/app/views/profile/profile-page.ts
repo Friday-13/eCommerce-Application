51a9f45587dc76ad422c0577323b4ba05a2f70d4
Anna Golosova <a.golosova@3l.ru>
2024-06-04 18:46:59 +0300

    feat: add updating user details

--- src/app/views/profile/profile-page.ts
+++ src/app/views/profile/profile-page.ts
@@ -17,6 +17,12 @@ import { IFormInputField, createInputField } from '@utils/create-input-field';
 import atLeastOneCharacter from '@utils/validators/at-least-one-character-validator';
 import noSpecialCharacterOrNumber from '@utils/validators/no-special-characters-or-numbers-validator';
 import { CheckboxComponent } from '@components/checkbox-component';
+import createPasswordField from '@utils/create-password-field';
+import {
+  passwordValidator,
+  specialCharValidator,
+} from '@utils/validators/password-validator';
+import { IButtonAttributes } from '@components/button-component';
 
 export default class ProfileView extends View {
   private form = new FormComponent({});
@@ -45,6 +51,14 @@ export default class ProfileView extends View {
 
   private billingAndShipping = new FormSectionView();
 
+  private passwordSection = new FormSectionView();
+
+  private currentPasswordInput = new InputFieldComponent({}, {}, {});
+
+  private newPasswordInput = new InputFieldComponent({}, {}, {});
+
+  private confirmPasswordInput = new InputFieldComponent({}, {}, {});
+
   constructor() {
     const attrs: IAttributes = {
       classList: 'row',
@@ -54,6 +68,9 @@ export default class ProfileView extends View {
     this.addForm();
     this.populateProfileView();
     this.addPersonalInformation();
+    this.addPasswordFields();
+    this.addEditButton();
+    this.addSaveButton();
   }
 
   private addForm() {
@@ -124,6 +141,40 @@ export default class ProfileView extends View {
     this.pesonalSection.appendChild(this.lastNameInput);
     this.pesonalSection.appendChild(this.birthdayInput);
     this.form.node.appendChild(this.pesonalSection.htmlElement);
+  }
+
+  private addPasswordFields() {
+    this.passwordSection = new FormSectionView('Password Management');
+    this.currentPasswordInput = createPasswordField({
+      label: 'Current password',
+      id: 'new-password',
+      placeholder: 'Enter your current password',
+      type: 'password',
+      customValidators: [passwordValidator, specialCharValidator],
+    });
+    this.newPasswordInput = createPasswordField({
+      label: 'New password',
+      id: 'new-password',
+      placeholder: 'Enter your new password',
+      type: 'password',
+      customValidators: [passwordValidator, specialCharValidator],
+    });
+    this.confirmPasswordInput = createPasswordField({
+      label: 'Confirm new password',
+      id: 'new-password',
+      placeholder: 'Enter your confirm new password',
+      type: 'password',
+      customValidators: [passwordValidator, specialCharValidator],
+    });
+
+    this.currentPasswordInput.input.setDisable(true);
+    this.newPasswordInput.input.setDisable(true);
+    this.confirmPasswordInput.input.setDisable(true);
+
+    this.passwordSection.appendChild(this.currentPasswordInput);
+    this.passwordSection.appendChild(this.newPasswordInput);
+    this.passwordSection.appendChild(this.confirmPasswordInput);
+    this.form.node.appendChild(this.passwordSection.htmlElement);
   }
 
   private populateAddresses(customerData: Customer | null): void {
@@ -269,5 +320,105 @@ export default class ProfileView extends View {
     };
     this.countryInput = createInputField(attrs);
     container.appendChild(this.countryInput);
+  }
+
+  public enableEditPassword() {
+    this.currentPasswordInput.input.setDisable(false);
+    this.newPasswordInput.input.setDisable(false);
+    this.confirmPasswordInput.input.setDisable(false);
+  }
+
+  public enableEditPersonal() {
+    this.firstNameInput.input.setDisable(false);
+    this.lastNameInput.input.setDisable(false);
+    this.birthdayInput.input.setDisable(false);
+  }
+
+  public enableEditAddress() {
+    this.streetInput.input.setDisable(false);
+    this.cityInput.input.setDisable(false);
+    this.postalCodeInput.input.setDisable(false);
+  }
+
+  public enableSavePassword() {
+    this.currentPasswordInput.input.setDisable(true);
+    this.newPasswordInput.input.setDisable(true);
+    this.confirmPasswordInput.input.setDisable(true);
+  }
+
+  public enableSavePersonal() {
+    this.firstNameInput.input.setDisable(true);
+    this.lastNameInput.input.setDisable(true);
+    this.birthdayInput.input.setDisable(true);
+  }
+
+  public enableSaveAddress() {
+    this.streetInput.input.setDisable(true);
+    this.cityInput.input.setDisable(true);
+    this.postalCodeInput.input.setDisable(true);
+  }
+
+  private addEditButton() {
+    const buttonEditAttr: IButtonAttributes = {
+      classList: [
+        'edit-profile-button',
+        'profile-button',
+        'waves-effect',
+        'waves-light',
+        'btn',
+        'no-text-transform',
+      ],
+      content: 'Edit',
+    };
+    const buttonEditPassword = new BaseComponent(buttonEditAttr);
+    const buttonEditPersonal = new BaseComponent(buttonEditAttr);
+    const buttonEditAddress = new BaseComponent(buttonEditAttr);
+    buttonEditPassword.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableEditPassword();
+    });
+    buttonEditPersonal.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableEditPersonal();
+    });
+    buttonEditAddress.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableEditAddress();
+    });
+    this.passwordSection.appendChild(buttonEditPassword);
+    this.pesonalSection.appendChild(buttonEditPersonal);
+    this.form.appendChild(buttonEditAddress);
+  }
+
+  private addSaveButton() {
+    const buttonSaveAttr: IButtonAttributes = {
+      classList: [
+        'save-profile-button',
+        'profile-button',
+        'waves-effect',
+        'waves-light',
+        'btn',
+        'no-text-transform',
+      ],
+      content: 'Save',
+    };
+    const buttonSavePassword = new BaseComponent(buttonSaveAttr);
+    const buttonSavePersonal = new BaseComponent(buttonSaveAttr);
+    const buttonSaveAddress = new BaseComponent(buttonSaveAttr);
+    buttonSavePassword.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableSavePassword();
+    });
+    buttonSavePersonal.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableSavePersonal();
+    });
+    buttonSaveAddress.node.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.enableSaveAddress();
+    });
+    this.passwordSection.appendChild(buttonSavePassword);
+    this.pesonalSection.appendChild(buttonSavePersonal);
+    this.form.appendChild(buttonSaveAddress);
   }
 }
