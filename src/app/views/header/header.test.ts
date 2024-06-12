2e2913be4a94881f6fa7f69380299ad03793c928
dashaborzova <dashaborzova@yandex.ru>
2024-06-13 02:29:08 +0300

    fix(header): header.test fix

--- src/app/views/header/header.test.ts
+++ src/app/views/header/header.test.ts
@@ -18,9 +18,9 @@ describe('HeaderView', () => {
       const menuItems = navElement.querySelectorAll('li');
       expect(menuItems.length).toBe(5);
       expect(menuItems[0].textContent).toContain('Catalog');
-      expect(menuItems[2].textContent).toContain('Sign in');
-      expect(menuItems[3].textContent).toContain('Sign up');
-      expect(menuItems[1].textContent).toContain('About Us');
+      expect(menuItems[1].textContent).toContain('Sign in');
+      expect(menuItems[2].textContent).toContain('Sign up');
+      expect(menuItems[3].textContent).toContain('About Us');
       expect(menuItems[4].textContent).toContain('Cart');
     } else {
       throw new Error('Navigation element is not found');
