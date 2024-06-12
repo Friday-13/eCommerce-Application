c7bd3282d53ff726f80c4b2e3c0d93c390de5c95
dashaborzova <dashaborzova@yandex.ru>
2024-06-13 02:38:51 +0300

    fix(header): changed cart into shopping cart to pass

--- src/app/views/header/header.test.ts
+++ src/app/views/header/header.test.ts
@@ -21,7 +21,7 @@ describe('HeaderView', () => {
       expect(menuItems[1].textContent).toContain('Sign in');
       expect(menuItems[2].textContent).toContain('Sign up');
       expect(menuItems[3].textContent).toContain('group');
-      expect(menuItems[4].textContent).toContain('Cart');
+      expect(menuItems[4].textContent).toContain('shopping_cart');
     } else {
       throw new Error('Navigation element is not found');
     }
