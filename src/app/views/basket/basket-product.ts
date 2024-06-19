29c26607ffc07d4c103d0959b3b8da18f0b63191
Anna Golosova <a.golosova@3l.ru>
2024-06-19 17:53:35 +0300

    feat: add adaptive page about

--- src/app/views/basket/basket-product.ts
+++ src/app/views/basket/basket-product.ts
@@ -1,4 +1,5 @@
 import { BaseComponent, IAttributes } from '@components/base-component';
+import { IButtonAttributes } from '@components/button-component';
 import { IImageAttributes, ImageComponent } from '@components/image-component';
 import { ICartData, ILineItem } from '@models/cart';
 import cartHandler from '@services/cart-handler';
@@ -189,17 +190,39 @@ export default class BasketProductView extends View {
       productPrice.addClass('price-strikethrough');
     }
 
-    const { quantity } = this.productCartData.lineItem;
+    // добавляем блок для изменения количества
+    const blockCountContainerAttrs: IAttributes = {
+      classList: ['cart-block-count'],
+    };
+    const blockCountContainer = new BaseComponent(blockCountContainerAttrs);
+
+    const blockCountButtonMinusAttrs: IButtonAttributes = {
+      classList: ['cart-block-count-minus'],
+      content: '-',
+    };
+    const blockCountButtonMinus = new BaseComponent(blockCountButtonMinusAttrs);
 
     // Добавляем контейнер для количества
+    const { quantity } = this.productCartData.lineItem;
+
     const productCountAttrs: IAttributes = {
       classList: ['cart-product-quantity'],
       content: `${quantity}`,
     };
     const productCount = new BaseComponent(productCountAttrs);
 
+    const blockCountButtonPlusAttrs: IButtonAttributes = {
+      classList: ['cart-block-count-plus'],
+      content: '+',
+    };
+    const blockCountButtonPlus = new BaseComponent(blockCountButtonPlusAttrs);
+
+    blockCountContainer.appendChild(blockCountButtonMinus);
+    blockCountContainer.appendChild(productCount);
+    blockCountContainer.appendChild(blockCountButtonPlus);
+
     digitsTitleCart.appendChild(this.pricesContainer);
-    digitsTitleCart.appendChild(productCount);
+    digitsTitleCart.appendChild(blockCountContainer);
 
     // this.imageContainerCart.appendChild(imageProductCart);
 
